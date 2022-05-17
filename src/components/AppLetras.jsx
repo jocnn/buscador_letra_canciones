c9445290e4691b371081fbed5c015b2f615beec9
jocnn <ijann_@outlook.com>
2022-05-17 10:18:55 -0300

    creacion y uso de componentes

---
+++ src/components/AppLetras.jsx
@@ -0,0 +1,18 @@
+import Formulario from "./Formulario"
+
+const AppLetras = () => {
+  return (
+    <div>
+      <header>Búsqueda de letras de canciones</header>
+
+      <Formulario />
+
+      <main>
+        
+      </main>
+
+    </div>
+  )
+}
+
+export default AppLetras
