a53ea08d4426f419446c37376fb659be03d707fd
jocnn <ijann_@outlook.com>
2022-05-17 12:33:02 -0300

    definiendo diseño y el state de los campos

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,8 +1,51 @@
+import { useState } from "react"
+
 const Formulario = () => {
+  
+  const [ busqueda, setBusqueda ] = useState({
+    artista: '',
+    cancion: ''
+  })
+
+  const handleChangeBusqueda = e => {
+    setBusqueda({
+      ...busqueda,
+      [e.target.name]: e.target.value
+    })
+  }
+  
   return (
-    <>
-      <h1>Formulario</h1>
-    </>
+    <form>
+      <legend>Busca por Artistas y Canción</legend>
+
+      <div className="form-grid">
+        <div>
+          <label htmlFor="artista">Artista</label>
+          <input 
+            id="artista"
+            type="text" 
+            name="artista"
+            placeholder="Nombre Artista"
+            value={busqueda.artista}
+            onChange={ e => handleChangeBusqueda(e) }
+          />
+        </div>
+
+        <div>
+          <label htmlFor="cancion">Canción</label>
+          <input 
+            id="cancion"
+            type="text" 
+            name="cancion"
+            placeholder="Nombre Canción"
+            value={busqueda.cancion}
+            onChange={ e => handleChangeBusqueda(e) }
+          />
+        </div>
+
+        <input type="submit" value="Buscar" />
+      </div>
+    </form>
   )
 }
 
